9975a1a6d491d10875d3515878fd98aba686dff7
pszhuchkov <pzhuchkov@gmail.com>
2018-04-16 14:47:08 +0400

    Initial commit

---
+++ temperature_plot.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+The script builds graph of monthly average temperatures for period 2010-2017
+
+Author: Pavel Zhuchkov - 16.04.2018
+"""
+# Import modules
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read file
+dataHel = pd.read_csv(r"helsinki.csv")
+
+# Convert column to datetime type
+dataHel['YM'] = pd.to_datetime(dataHel['YM'],format='%Y%m')
+
+# Set datetime index
+dataHel = dataHel.set_index('YM')
+
+# Slice period 2010-2017
+period = dataHel['2010-01-01':'2017-01-01']
+
+# Plot
+plt.plot(period.index,period['TAVG_Celsius'],'ko--')
+plt.title('Monthly average temperatures in Helsinki for period 2010-2017')
+plt.xlabel('DATE')
+plt.ylabel('Temperature [°C]')
+plt.show()
